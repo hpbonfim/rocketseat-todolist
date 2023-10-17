520b1e3f5545fbfd67d0a25d20771f77ab1b10ee
Replit user <>
2023-10-17 02:28:12 +0000

    day 5

--- src/main/java/br/com/henriquebonfim/todolist/user/UserModel.java
+++ src/main/java/br/com/henriquebonfim/todolist/user/UserModel.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @Entity(name = "table_users")
